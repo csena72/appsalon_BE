826051c7bf0e2afc44909a17bed18f5694fc94ed
Cristian Sena <crisianjsena@gmail.com>
2024-08-19 12:56:44 -0300

    Agrego mongoose y credenciales db

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import { db } from './config/db.js';
+import servicesRoutes from './routes/servicesRoutes.js';
+import { configDotenv } from 'dotenv';
 
+
+configDotenv('./.env');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+db();
+
+app.use('/api/services', servicesRoutes);
 
 app.listen(port, () => {
     console.log(`Server listening at ${process.env.HOST}:${port}`); 
