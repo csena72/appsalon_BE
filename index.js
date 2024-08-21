4486f116ba8aebad788ab43772b538ebc55a85c5
Cristian Sena <crisianjsena@gmail.com>
2024-08-20 21:21:44 -0300

    Agrego CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import colors from 'colors';
+import cors from 'cors';
 import { db } from './config/db.js';
 import servicesRoutes from './routes/servicesRoutes.js';
 import { configDotenv } from 'dotenv';
@@ -11,6 +12,24 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 db();
+
+const whiteList = [process.env.FRONTEND_URL];
+
+if(process.argv[2] === '--postman') {
+    whiteList.push(undefined);
+}
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whiteList.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
